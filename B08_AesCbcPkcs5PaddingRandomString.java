0d04665fcfbd6e304be280dc1ad9b41f36e08562
Michael Fehr <javacrypto@gmx.de>
2018-11-07 18:10:06 +0100

    Add files via upload

---
+++ B08_AesCbcPkcs5PaddingRandomString.java
@@ -0,0 +1,105 @@
+package net.bplaced.javacrypto.symmetricencryption;
+
+/*
+* Herkunft/Origin: http://javacrypto.bplaced.net/
+* Programmierer/Programmer: Michael Fehr
+* Copyright/Copyright: frei verwendbares Programm (Public Domain)
+* Copyright: This is free and unencumbered software released into the public domain.
+* Lizenttext/Licence: <http://unlicense.org>
+* getestet mit/tested with: Java Runtime Environment 8 Update 191 x64
+* Datum/Date (dd.mm.jjjj): 01.11.2018 
+* Funktion: verschlüsselt einen string im aes cbc modus pkcs5 padding
+* Function: encrypts a string using aes cbc modus with pkcs5 padding
+*
+* Sicherheitshinweis/Security notice
+* Die Programmroutinen dienen nur der Darstellung und haben keinen Anspruch auf eine 
+* korrekte Funktion, insbesondere mit Blick auf die Sicherheit ! 
+* Prüfen Sie die Sicherheit bevor das Programm in der echten Welt eingesetzt wird.
+* The program routines just show the function but please be aware of the security part - 
+* check yourself before using in the real world !
+*/
+
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+
+public class B08_AesCbcPkcs5PaddingRandomString {
+
+	public static void main(String[] args)
+			throws UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException,
+			IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
+		System.out.println("B08 AES im Betriebsmodus CBC PKCS5Padding mit Zufalls-Initvektor mit einem String");
+		// es werden ein paar variablen benötigt:
+		String plaintextString = "HalloWelt012345"; // hier 15 zeichen
+		String decryptedtextString = ""; // enthält später den entschlüsselten text
+
+		// diese konstanten und variablen benötigen wir zur ver- und entschlüsselung
+		// der schlüssel ist exakt 32 zeichen lang und bestimmt die stärke der
+		// verschlüsselung
+		// mögliche schlüssellängen sind 16 byte (128 bit), 24 byte (192 bit) und 32
+		// byte (256 bit)
+		// final byte[] keyByte = "1234567890123456".getBytes("UTF-8"); // 16 byte
+		final byte[] keyByte = "12345678901234567890123456789012".getBytes("UTF-8"); // 32 byte
+		
+		// der initialisierungsvektor ist exakt 16 zeichen lang
+		final byte[] initvectorByte = new byte[16];
+		SecureRandom secureRandom = new SecureRandom();
+		secureRandom.nextBytes(initvectorByte);
+
+		byte[] plaintextByte = null;
+		// der verschluesselte (encrypted) text kommt in diese variable in form eines
+		// byte arrays
+		byte[] ciphertextByte = null; // die länge steht noch nicht fest, da sie von der größe des plaintextes abhängt
+		// der entschlüsselte (decrypted) text kommt in dieses byte array, welches
+		// später in einen string umkodiert wird
+		byte[] decryptedtextByte = null; // die länge steht noch nicht fest, da sie von der größe des plaintextes
+											// abhängt
+
+		// ab hier arbeiten wir nun im verschlüsselungsmodus
+		// umwandlung des klartextes in ein byte array
+		plaintextByte = plaintextString.getBytes("UTF-8");
+		// der schlüssel wird in die richtige form gebracht
+		SecretKeySpec keySpec = new SecretKeySpec(keyByte, "AES");
+		// der initvector wird in die richtige form gebracht
+		IvParameterSpec ivKeySpec = new IvParameterSpec(initvectorByte);
+		// die verschlüsselungsroutine wird mit dem gewünschten parameter erstellt
+		Cipher aesCipherEnc = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+		// nun wird die routine mit dem schlüssel initialisiert
+		aesCipherEnc.init(Cipher.ENCRYPT_MODE, keySpec, ivKeySpec);
+		// hier erfolgt nun die verschlüsselung des plaintextes
+		ciphertextByte = aesCipherEnc.doFinal(plaintextByte);
+
+		// ab hier arbeiten wir nun im entschlüsselungsmodus
+		// die verschlüsselungsroutine wird mit dem gewünschten parameter erstellt
+		Cipher aesCipherDec = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+		// zum einsatz kommt derselbe schlüssel, daher symmetrische verschlüsselung
+		// achtung: hier wird der DECRYPT_MODE = entschlüsselung genutzt
+		aesCipherDec.init(Cipher.DECRYPT_MODE, keySpec, ivKeySpec);
+		// nun wird der ciphertext wieder entschlüsselt
+		decryptedtextByte = aesCipherDec.doFinal(ciphertextByte);
+
+		// zurück-kodierung des byte array in text
+		decryptedtextString = new String(decryptedtextByte, "UTF-8");
+
+		// ausgabe der variablen
+		System.out.println("");
+		System.out.println("keyByte (hex)          :" + DatatypeConverter.printHexBinary(keyByte));
+		System.out.println("initvectorByte (hex)   :" + DatatypeConverter.printHexBinary(initvectorByte));
+		System.out.println("plaintextString        :" + plaintextString);
+		System.out.println("plaintextByte (hex)    :" + DatatypeConverter.printHexBinary(plaintextByte));
+		System.out.println("= = = Verschlüsselung = = =");
+		System.out.println("ciphertextByte (hex)   :" + DatatypeConverter.printHexBinary(ciphertextByte));
+		System.out.println("= = = Entschlüsselung = = =");
+		System.out.println("decryptedtextByte (hex):" + DatatypeConverter.printHexBinary(decryptedtextByte));
+		System.out.println("decryptedtextString    :" + decryptedtextString);
+	}
+}
---
+++ B09b_AesCbcPkcs5PaddingRandomFile.java
@@ -0,0 +1,207 @@
+package net.bplaced.javacrypto.symmetricencryption;
+
+/*
+* Herkunft/Origin: http://javacrypto.bplaced.net/
+* Programmierer/Programmer: Michael Fehr
+* Copyright/Copyright: frei verwendbares Programm (Public Domain)
+* Copyright: This is free and unencumbered software released into the public domain.
+* Lizenttext/Licence: <http://unlicense.org>
+* getestet mit/tested with: Java Runtime Environment 8 Update 191 x64
+* Datum/Date (dd.mm.jjjj): 01.11.2018 
+* Funktion: liest eine datei und verschlüsselt sie im aes cbc modus pkcs5 padding
+* Function: encrypts a file using aes cbc modus with pkcs5 padding
+*
+* Sicherheitshinweis/Security notice
+* Die Programmroutinen dienen nur der Darstellung und haben keinen Anspruch auf eine 
+* korrekte Funktion, insbesondere mit Blick auf die Sicherheit ! 
+* Prüfen Sie die Sicherheit bevor das Programm in der echten Welt eingesetzt wird.
+* The program routines just show the function but please be aware of the security part - 
+* check yourself before using in the real world !
+*/
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+
+public class B09b_AesCbcPkcs5PaddingRandomFile {
+
+	public static void main(String[] args)
+			throws UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException,
+			IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
+		System.out.println("B09b AES im Betriebsmodus CBC PKCS5Padding mit Zufalls-Initvektor mit einer Datei");
+		// es werden ein paar variablen benötigt:
+		String dateinameReadString = "b09_test.txt"; // aus der datei wird das plaintextByte eingelesen
+		String dateinameWriteString = "b09b_test.enc"; // in diese datei wird das ciphertextByte geschrieben
+
+		String plaintextString = ""; // die daten werden aus der datei gelesen
+		byte[] plaintextByte = null; // die daten werden aus der datei gelesen
+
+		String decryptedtextString = ""; // enthält später den entschlüsselten text
+
+		// zuerst testen wir ob die einzulesende datei existiert
+		if (FileExistsCheck(dateinameReadString) == false) {
+			System.out.println("Die Datei " + dateinameReadString + " existiert nicht. Das Programm wird beendet.");
+			System.exit(0);
+		};
+
+		// datei in byte array einlesen
+		plaintextByte = readBytesFromFileNio(dateinameReadString);
+		plaintextString = new String(plaintextByte, "UTF-8"); // die umwandlung erfolgt nur zur späteren anzeige
+
+		// diese konstanten und variablen benötigen wir zur ver- und entschlüsselung
+		// der schlüssel ist exakt 32 zeichen lang und bestimmt die stärke der
+		// verschlüsselung
+		// hier ist der schlüssel 32 byte = 256 bit lang
+		// mögliche schlüssellängen sind 16 byte (128 bit), 24 byte (192 bit) und 32
+		// byte (256 bit)
+		// final byte[] keyByte = "1234567890123456".getBytes("UTF-8"); // 16 byte
+		final byte[] keyByte = "12345678901234567890123456789012".getBytes("UTF-8"); // 32 byte
+		// der initialisierungsvektor ist exakt 16 zeichen lang
+		final byte[] initvectorByte = new byte[16];
+		SecureRandom secureRandom = new SecureRandom();
+		secureRandom.nextBytes(initvectorByte);
+
+		// der verschluesselte (encrypted) text kommt in diese variable in form eines
+		// byte arrays
+		byte[] ciphertextByte = null; // die länge steht noch nicht fest, da sie von der größe des plaintextes abhängt
+		// der entschlüsselte (decrypted) text kommt in dieses byte array, welches
+		// später in einen string umkodiert wird
+		byte[] decryptedtextByte = null; // die länge steht noch nicht fest, da sie von der größe des plaintextes
+											// abhängt
+
+		// ab hier arbeiten wir nun im verschlüsselungsmodus
+		// umwandlung des klartextes in ein byte array
+		plaintextByte = plaintextString.getBytes("UTF-8");
+		// hier erfolgt nun die verschlüsselung des plaintextes
+		ciphertextByte = AesCbcPaddingEncrypt(plaintextByte, keyByte, initvectorByte);
+
+		// byte array in eine datei schreiben
+		writeBytesToFileNio(ciphertextByte, dateinameWriteString);
+
+		System.out.println("");
+		System.out.println("Klartextdaten einlesen und als verschlüsselte Datei speichern");
+		System.out.println("keyByte (hex)          :" + DatatypeConverter.printHexBinary(keyByte));
+		System.out.println("initvectorByte (hex)   :" + DatatypeConverter.printHexBinary(initvectorByte));
+		System.out.println("Dateiname Lesen        :" + dateinameReadString);
+		System.out.println("Dateiname Schreiben    :" + dateinameWriteString);
+		System.out.println("plaintextByte (hex)    :" + DatatypeConverter.printHexBinary(plaintextByte));
+		System.out.println("plaintextString        :" + plaintextString);
+		System.out.println("= = = Verschlüsselung = = =");
+		System.out.println("ciphertextByte (hex)   :" + DatatypeConverter.printHexBinary(ciphertextByte));
+
+		// ab hier arbeiten wir nun im entschlüsselungsmodus
+		// wir starten die entschlüsselung mit einem leeren ciphertext
+		ciphertextByte = null;
+
+		// byte array einlesen
+		dateinameReadString = "b09b_test.enc"; // ciphertextByte lesen
+		dateinameWriteString = "b09b_test.dec"; // decryptedtextByte schreiben
+
+		// zuerst testen wir ob die einzulesende datei existiert
+		if (FileExistsCheck(dateinameReadString) == false) {
+			System.out.println("Die Datei " + dateinameReadString + " existiert nicht. Das Programm wird beendet.");
+			System.exit(0);
+		};
+		
+		// das ciphertextByte wird aus der datei gelesen
+		ciphertextByte = readBytesFromFileNio(dateinameReadString);
+
+		// nun wird der ciphertext wieder entschlüsselt
+		decryptedtextByte = AesCbcPaddingDecrypt(ciphertextByte, keyByte, initvectorByte);
+
+		// wir schreiben die entschlüsselten daten in eine datei
+		writeBytesToFileNio(decryptedtextByte, dateinameWriteString);
+
+		// zurück-kodierung des byte array in text
+		decryptedtextString = new String(decryptedtextByte, "UTF-8");
+
+		// ausgabe der variablen
+		System.out.println("");
+		System.out.println("keyByte (hex)          :" + DatatypeConverter.printHexBinary(keyByte));
+		System.out.println("Dateiname Lesen        :" + dateinameReadString);
+		System.out.println("Dateiname Schreiben    :" + dateinameWriteString);
+		System.out.println("initvectorByte (hex)   :" + DatatypeConverter.printHexBinary(initvectorByte));
+		System.out.println("plaintextString        :" + plaintextString);
+		System.out.println("plaintextByte (hex)    :" + DatatypeConverter.printHexBinary(plaintextByte));
+		System.out.println("= = = Verschlüsselung = = =");
+		System.out.println("ciphertextByte (hex)   :" + DatatypeConverter.printHexBinary(ciphertextByte));
+		System.out.println("= = = Entschlüsselung = = =");
+		System.out.println("decryptedtextByte (hex):" + DatatypeConverter.printHexBinary(decryptedtextByte));
+		System.out.println("decryptedtextString    :" + decryptedtextString);
+	}
+	
+	public static byte[] AesCbcPaddingEncrypt(byte[] plaintextByte, byte[] keyByte, byte[] initvectorByte)
+			throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException,
+			InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+		// der genutzte algorithmus
+		final String AESMODE_CBC_PKCS5PADDING = "AES/CBC/PKCS5PADDING";
+		byte[] ciphertextByte = null;
+		// der schlüssel wird in die richtige form gebracht
+		SecretKeySpec keySpec = new SecretKeySpec(keyByte, "AES");
+		// der initvector wird in die richtige form gebracht
+		IvParameterSpec ivKeySpec = new IvParameterSpec(initvectorByte);
+		// die verschlüsselungsroutine wird mit dem gewünschten parameter erstellt
+		Cipher aesCipherEnc = Cipher.getInstance(AESMODE_CBC_PKCS5PADDING);
+		// nun wird die routine mit dem schlüssel initialisiert
+		aesCipherEnc.init(Cipher.ENCRYPT_MODE, keySpec, ivKeySpec);
+		// hier erfolgt nun die verschlüsselung des plaintextes
+		ciphertextByte = aesCipherEnc.doFinal(plaintextByte);
+		return ciphertextByte;
+	}
+
+	public static byte[] AesCbcPaddingDecrypt(byte[] ciphertextByte, byte[] keyByte, byte[] initvectorByte)
+			throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException,
+			InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+		// der genutzte algorithmus
+		final String AESMODE_CBC_PKCS5PADDING = "AES/CBC/PKCS5PADDING";
+		byte[] decryptedtextByte = null;
+		// der schlüssel wird in die richtige form gebracht
+		SecretKeySpec keySpec = new SecretKeySpec(keyByte, "AES");
+		// der initvector wird in die richtige form gebracht
+		IvParameterSpec ivKeySpec = new IvParameterSpec(initvectorByte);
+		// die verschlüsselungsroutine wird mit dem gewünschten parameter erstellt
+		Cipher aesCipherDec = Cipher.getInstance(AESMODE_CBC_PKCS5PADDING);
+		// nun wird die routine mit dem schlüssel initialisiert
+		aesCipherDec.init(Cipher.DECRYPT_MODE, keySpec, ivKeySpec);
+		// hier erfolgt nun die verschlüsselung des plaintextes
+		decryptedtextByte = aesCipherDec.doFinal(ciphertextByte);
+		return decryptedtextByte;
+	}
+
+	private static boolean FileExistsCheck(String dateinameString) {
+		return Files.exists(Paths.get(dateinameString), new LinkOption[] { LinkOption.NOFOLLOW_LINKS });
+	}
+
+	private static void writeBytesToFileNio(byte[] byteToFileByte, String filenameString) {
+		try {
+			Path path = Paths.get(filenameString);
+			Files.write(path, byteToFileByte);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static byte[] readBytesFromFileNio(String filenameString) {
+		byte[] byteFromFileByte = null;
+		try {
+			byteFromFileByte = Files.readAllBytes(Paths.get(filenameString));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return byteFromFileByte;
+	}
+}
